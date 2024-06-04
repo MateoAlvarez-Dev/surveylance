d9b3e282b76ac784c4d60c8c61316731c5fff9d3
MateoAlvarez-Dev <matheoz2003@gmail.com>
2024-06-04 13:39:35 -0500

    Open API config

--- src/main/java/com/riwi/surveylance/config/OpenApiConfig.java
+++ src/main/java/com/riwi/surveylance/config/OpenApiConfig.java
@@ -3,13 +3,30 @@ package com.riwi.surveylance.config;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 /**
  * Configuracion de swagger
  */
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Surveylance API", version = "0", description = "All the Endpoints available of the API"))
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Mateo Alvarez Yepes",
+                        url = "https://github.com/MateoAlvarez-Dev",
+                        email = "matheoz2003@gmail.com"
+                ),
+                title = "Documentation: Surveylance API",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local",
+                        url = "http://localhost:8080/api/v1/surveylance")
+        }
+)
 public class OpenApiConfig {
 
 }
