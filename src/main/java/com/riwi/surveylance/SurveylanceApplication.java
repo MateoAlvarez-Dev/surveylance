8b677694afaeac96cd16b64ec550989d7cf8b4e1
MateoAlvarez-Dev <matheoz2003@gmail.com>
2024-06-04 07:16:01 -0500

    FIrst Commit

---
+++ src/main/java/com/riwi/surveylance/SurveylanceApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.surveylance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveylanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveylanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/surveylance/SurveylanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.surveylance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveylanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
